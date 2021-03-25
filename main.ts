47a3642a8e01b9aa23a0e7755340bb3afa3c2746
Witthawin <61728000+CVBirdNight724@users.noreply.github.com>
2021-03-25 11:48:58 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let y = 0
+let x = 0
+let ran = 0
+let score = 0
+basic.forever(function () {
+    basic.pause(randint(500, 1000))
+    ran = randint(0, 1)
+    if (ran == 0) {
+        x = randint(0, 1)
+        y = randint(0, 4)
+    } else {
+        x = randint(3, 4)
+        y = randint(0, 4)
+    }
+    led.plot(x, y)
+    for (let index = 0; index <= 1500; index++) {
+        for (let index = 0; index <= 1500; index++) {
+            if (input.buttonIsPressed(Button.A) && ran == 0) {
+                score += 1
+                basic.showNumber(score)
+            } else if (input.buttonIsPressed(Button.B) && ran == 1) {
+                score += 1
+                basic.showNumber(score)
+            }
+        }
+    }
+    basic.clearScreen()
+})
